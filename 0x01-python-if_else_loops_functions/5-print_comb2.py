569fb1b4ff8b45c14de417fe471aeac78771ccd2
fawaaaz111 <fawaztwix111@gmail.com>
2023-01-24 11:37:33 +0000

    Write a program that prints numbers from 0 to 99.

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for num in range(0, 100): #number
+for num in range(0, 100):  # number
     # get the first and second part of a number
     first, second = num / 10, num % 10
     if num != 99:
